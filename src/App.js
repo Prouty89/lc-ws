b92e0cd42c7b6e1b835110da96b1acdc23121cbd
Blake <bpro0821@gmail.com>
2020-04-12 16:13:15 -0500

    add load animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import loadable from '@loadable/component';
+import pMinDelay from 'p-min-delay';
 
 import { ThemeProvider } from 'styled-components';
 
-import Landing from './components/pages/Landing';
 import Commercial from './components/portfolio/Commerial';
 import Residential from './components/portfolio/Residential';
 
+import logoWhite from './Assets/logoWhite.svg';
+
+export const Landing = loadable(() =>
+  pMinDelay(import('./components/pages/Landing'), 1000), {
+    fallback: 
+    <div className="loading-screen">
+      <img className="logo-1" src={logoWhite} alt="logo-white"/>
+      <div class="spinner">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+      </div>
+    </div>
+  }
+)
 
 
 function App() {
