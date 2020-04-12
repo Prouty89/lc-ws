e830ee2b5a68e87d6a7a773b3398e231ae3e1947
Blake <bpro0821@gmail.com>
2020-04-11 19:24:52 -0500

    adds footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,8 +7,23 @@ const Footer = () => {
     return (
         <StyledFooter>
            
-            <div className="footer-container"> 
-                Hi There
+            <div className="footer-container">
+                <div className="links_footer">
+                <p className="link">
+                    Account
+                </p>
+                <p className="links">
+                    Portfolio
+                </p>
+                </div> 
+                <div className="social">
+                    <a href="www.facebook.com" target="__blank">
+                        <img className="facebook" src="https://img.icons8.com/ios/40/3d4839/facebook-new.png" alt="facebook"/>
+                    </a>
+                    <a href="www.facebook.com" target="__blank">
+                        <img className="insta" src="https://img.icons8.com/ios/40/3d4839/instagram-new.png" alt="instagram"/>
+                    </a>
+                    </div>
             </div> 
         </StyledFooter>
     )
--- src/components/footer/FooterStyles.js
+++ src/components/footer/FooterStyles.js
@@ -5,7 +5,30 @@ export const StyledFooter = styled.div`
 width: 92%;
 margin: 0 auto;
 background: ${props => props.theme.colors.bgWhite};
-height: 75px;
+height: 105px;
+
+.footer-container{
+    display: flex;
+    justify-content: space-between;
+}
+
+.links_footer{
+    display: flex;
+    color: ${props => props.theme.font.linkDark};
+    font-family: 'inter', sans-serif;
+    font-size: 1.3rem;
+    font-weight: bold;
+    width: 200px;
+    justify-content: space-between;
+    margin-left: 45px;
+}
+
+.social{
+    width: 90px;
+    display: flex;
+    margin-right: 35px;
+    justify-content: space-between;
+}
 
 
 `
