75733ff6cef6b8eee1f40037291f62209e572948
Blake <bpro0821@gmail.com>
2020-04-14 14:58:17 -0500

    test for lighthouse

--- src/components/images/ImageLoader.js
+++ src/components/images/ImageLoader.js
@@ -2,64 +2,16 @@ function getPhotos() {
     return [{
       src: 'https://www.datocms-assets.com/18044/1586887792-kjit2ktcspsfkudpzgoxrgthumb82-edited.png',
       alt: 'kitchen',
-      effect: 'opacity'
     }, {
-      src: 'https://www.datocms-assets.com/18044/1586886855-weltxow0rps6cfpt8fb1xathumb42-edited-edited.png'
+      src: 'https://www.datocms-assets.com/18044/1586886855-weltxow0rps6cfpt8fb1xathumb42-edited-edited.png',
+      alt: 'bathroom',
     }, {
-      src: 'https://www.datocms-assets.com/18044/1586880971-g71ybxq4tkc1h5i4bvccbathumb58-edited.png'
+      src: 'https://www.datocms-assets.com/18044/1586880971-g71ybxq4tkc1h5i4bvccbathumb58-edited.png',
+      alt: 'bathroom1'
     }, {
-      src: 'https://www.datocms-assets.com/18044/1586891941-c4-edited.png'
-    },
-    {
-        src: 'https://www.datocms-assets.com/18044/1586887792-kjit2ktcspsfkudpzgoxrgthumb82-edited.png',
-        alt: 'kitchen'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586886855-weltxow0rps6cfpt8fb1xathumb42-edited-edited.png'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586880971-g71ybxq4tkc1h5i4bvccbathumb58-edited.png'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586891941-c4-edited.png'
-      },
-      {
-        src: 'https://www.datocms-assets.com/18044/1586887792-kjit2ktcspsfkudpzgoxrgthumb82-edited.png',
-        alt: 'kitchen'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586886855-weltxow0rps6cfpt8fb1xathumb42-edited-edited.png'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586880971-g71ybxq4tkc1h5i4bvccbathumb58-edited.png'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586891941-c4-edited.png'
-      },
-      {
-        src: 'https://www.datocms-assets.com/18044/1586887792-kjit2ktcspsfkudpzgoxrgthumb82-edited.png',
-        alt: 'kitchen'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586886855-weltxow0rps6cfpt8fb1xathumb42-edited-edited.png'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586880971-g71ybxq4tkc1h5i4bvccbathumb58-edited.png'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586891941-c4-edited.png'
-      },
-      {
-        src: 'https://www.datocms-assets.com/18044/1586887792-kjit2ktcspsfkudpzgoxrgthumb82-edited.png',
-        alt: 'kitchen'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586886855-weltxow0rps6cfpt8fb1xathumb42-edited-edited.png'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586880971-g71ybxq4tkc1h5i4bvccbathumb58-edited.png'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586891941-c4-edited.png'
-      },
-      {
-        src: 'https://www.datocms-assets.com/18044/1586887792-kjit2ktcspsfkudpzgoxrgthumb82-edited.png',
-        alt: 'kitchen'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586886855-weltxow0rps6cfpt8fb1xathumb42-edited-edited.png'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586880971-g71ybxq4tkc1h5i4bvccbathumb58-edited.png'
-      }, {
-        src: 'https://www.datocms-assets.com/18044/1586891941-c4-edited.png'
-      }];
+      src: 'https://www.datocms-assets.com/18044/1586891941-c4-edited.png',
+      alt: 'bedroom'
+    }];
   }
   
   export default getPhotos;
